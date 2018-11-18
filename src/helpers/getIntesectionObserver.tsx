62d21b1fb58026489c19328bc75b3ce9e5d1ef5c
Tylor Shin <shincode@icloud.com>
2018-11-18 18:54:40 +0900

    Remove polyfill again...

--- src/helpers/getIntesectionObserver.tsx
+++ src/helpers/getIntesectionObserver.tsx
@@ -1,13 +1,7 @@
 export default function getIntersectionObserver():
   | typeof IntersectionObserver
   | null {
-  if (
-    typeof window !== "undefined" &&
-    typeof IntersectionObserver === "undefined"
-  ) {
-    // TODO: Optimize polyfill logic
-    console.log("REQUIRING POLYFILL FIRED");
-    require("intersection-observer");
+  if (typeof window !== "undefined") {
     return IntersectionObserver;
   }
   return null;
